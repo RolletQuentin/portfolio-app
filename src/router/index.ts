d98ce391fae2f2f33ebd58dbba53ff017f22de92
RolletQuentin <rollet.quentin@orange.fr>
2023-09-14 16:33:12 +0200

    change base rooter

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
     {
-        path: "/portfolio-app",
+        path: "/portfolio",
         component: () => import("@/layouts/default/Default.vue"),
         children: [
             {
