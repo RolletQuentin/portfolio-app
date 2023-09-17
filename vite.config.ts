a7d90331af4a8e3b61dcbcc699cffa50ce17a1c1
Quentin Rollet <rollet.quentin@orange.fr>
2023-09-17 13:58:04 +0200

    build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/portfolio",
+    base: "/",
     plugins: [
         vue({
             template: { transformAssetUrls },
